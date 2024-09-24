f677284d5ac60163e8eca502e4db213bbe833f45
Jake <jacob.watson00@yahoo.com>
2024-09-23 23:02:17 -0600

    added code to the markdown js file, as well as buttoned up some code and functions to pass through the terminal to create a test.md

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
 // TODO: Include packages needed for this application
+import fs from 'fs/promises';
 
+import inquirer from 'inquirer';
+import generateMarkdown from './utils/generateMarkdown.js';
 // TODO: Create an array of questions for user input
 const questions = [
-    
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'question 2'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose your license.',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC', 'None'],
+    },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+async function writeToFile(fileName, data) {
+    try {
+    await fs.writeFile(fileName, data);
+        console.log('README.md has been created!');
+    } catch (error) {
+        console.error('Error writing README.md: ', error);
+    }
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    try {
+        const answers = await inquirer.prompt(questions);
+        console.log(answers);
+        writeToFile('test.md', generateMarkdown(answers));
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 // Function call to initialize app
 init();
---
+++ test.md
@@ -0,0 +1,51 @@
+# another test
+
+here we try another test
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Description
+here we try another test
+
+## Installation
+To install the necessary dependencies, run the following command:
+
+```
+npm install
+```
+
+## Usage
+To generate a README file, simply run:
+
+```
+node index.js
+```
+
+Follow the prompts to enter project details like title, description, license, and more.
+
+  
+    https://opensource.org/licenses/Apache-2.0
+    ![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)
+
+## Contributing
+If you'd like to contribute to this project, please fork the repository and submit a pull request. For any major changes, please open an issue first to discuss what you'd like to change.
+
+## Tests
+To run tests, use the following command:
+
+```
+npm test
+```
+
+## Questions
+If you have any questions about the project, feel free to contact me:
+
+- GitHub: [undefined](https://github.com/undefined)
+- Email: undefined
